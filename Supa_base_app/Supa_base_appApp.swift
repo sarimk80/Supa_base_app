df372079bb5e8d30c88e759a38866cec41f945e6
Sarim khan <sarimk80@gmail.com>
2023-09-13 10:44:46 +0500

    Initial Commit

---
+++ Supa_base_app/Supa_base_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Supa_base_appApp.swift
+//  Supa_base_app
+//
+//  Created by sarim khan on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Supa_base_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
